e4613db3898b46b05c355375189dff88e7f06508
litneet64 <litneet64@gmail.com>
2021-02-23 04:31:39 -0300

    wip: add extractor class into main lib

---
+++ etherblob/lib/extractor.py
@@ -0,0 +1,23 @@
+
+import magic
+from math import log, ceil
+
+class Extractor():
+    def __init__():
+        pass
+
+    def extract_from_transactions():
+        # iterate all over the transactions from that block
+        block_trans = block_info.get('transactions')
+        for trans_obj in block_trans:
+            trans_hash = trans_obj['hash']
+            trans_data = trans_obj['input']
+
+
+        return
+
+    def extract_from_trans_address():
+        return
+
+    def extract_from_block():
+        return
