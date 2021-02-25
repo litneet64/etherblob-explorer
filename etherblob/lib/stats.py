ef3893d17752c845334bc65fdc4d1b19d119c8eb
litneet64 <litneet64@gmail.com>
2021-02-25 05:34:11 -0300

    wip: add final metrics method and minor details

--- etherblob/lib/stats.py
+++ etherblob/lib/stats.py
@@ -6,14 +6,21 @@ class Stats():
     def __init__(blob_exp):
         self.logger = blob_exp.logger
         self.blob_explorer = blob_exp
-        self.total_blocks = 
-        self.last_time = -1
+        self.total_blocks = blob_exp.args.end_block - blob_exp.args.start_block
+        self.last_time = time()
 
-    # get overall progress metrics since a certain time
-    def get_metrics(self):
+    # show overall progress metrics since a certain time
+    def show_cycle_metrics(self):
         if (time() - self.last_time) >= self.WAIT_TIME:
             self.logger.info(f"Parsed {block_id - s_block}/{self.total_blocks} \
                             blocks and {trans_c} transactions...")
-            last_t = time()
+            self.last_time = time()
 
-        return last_time
+        return
+
+    # show final metrics
+    def show_final_metrics(self):
+        logging.info(f"Finished downloading/reviewing all {total_blocks} blocks!")
+        logging.info(f"Total number of transactions {trans_c}!")
+
+        return
