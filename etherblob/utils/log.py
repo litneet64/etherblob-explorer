51a40dc1a26e337b757f23c749c91ac5e258a1a5
litneet64 <litneet64@gmail.com>
2021-02-25 05:32:24 -0300

    feat: add minor details to logging class

--- etherblob/utils/log.py
+++ etherblob/utils/log.py
@@ -3,16 +3,21 @@ import sys
 
 class Logger():
     FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
+    OUT_LOG = "etherblob_{}-{}.log"
+    logger = None
 
     # setup logging config for stdout and a file
     @classmethod
     def logging_setup(cls, s_block, e_block, out_log):
+        if out_log == "default_log_file":
+            out_log = cls.OUT_LOG.format(s_block, e_block)
+
         # set formatter and get root logger
         log_fmt = logging.Formatter(cls.FORMAT)
         root_log = logging.getLogger()
 
         # create file handler and attach to root logger
-        file_hdlr = logging.FileHandler(out_log.format(s_block, e_block))
+        file_hdlr = logging.FileHandler(out_log)
         file_hdlr.setFormatter(log_fmt)
         file_hdlr.setLevel(logging.INFO)
         root_log.addHandler(file_hdlr)
@@ -25,10 +30,13 @@ class Logger():
 
         root_log.setLevel(logging.INFO)
 
+        # save inside class
+        self.logger = root_log
+
         return root_log
 
     # wrapper around exit with logging message
-    @staticmethod
-    def error_exit():
-        logging.error("Exiting...")
+    @classmethod
+    def error_exit(cls):
+        self.logger.error("Exiting...")
         exit(127)
