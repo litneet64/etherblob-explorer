7b0189a19d6928a6563b666e52cf502830d7c89b
litneet64 <litneet64@gmail.com>
2021-02-27 03:51:45 -0300

    fix: minor bugs on log strings

--- etherblob/lib/stats.py
+++ etherblob/lib/stats.py
@@ -12,23 +12,30 @@ class Stats():
         self.last_time = time()
         self.last_blk_n = 0
 
+        # counters for interesting stats
+        self.files_c = 0
+        self.trans_c = 0
+        self.addr_file_c = 0
+
         # message to show every 60s
         self.cycle_msg = f"Parsed {{}}/{self.total_blocks} blocks ({{}} [block]/[min]), "
-        self.cycle_msg += f" found {self.blob_exp.files_c} files so far"
+        self.cycle_msg += "found {} files so far"
+
 
     # show overall progress metrics since a certain time
     def show_cycle_metrics(self):
         if (time() - self.last_time) >= self.WAIT_TIME:
             # format template message with dynamic block data
-            curr_blk = self.blob_exp.block_id - self.blob_exp.start_block
-            msg = self.cycle_msg.format(curr_blk, curr_blk - self.last_blk_n)
+            curr_blk = self.blob_exp.block_id - self.blob_exp.args.start_block
+            msg = self.cycle_msg.format(curr_blk, curr_blk - self.last_blk_n,
+                                        self.files_c)
 
             # append data to cycle message with correct args according to extraction mode
             if self.blob_exp.args.transactions:
-                msg += f" and {self.blob_exp.trans_c} transactions"
+                msg += f" and {self.trans_c} transactions"
             if self.blob_exp.args.addresses:
-                msg += f" {self.blob_exp.trans_c} transactions, and \
-                        {len(self.blob_exp.extractor.tracked_addr)} interesting addresses"
+                msg += f" {self.trans_c} transactions, and "\
+                        f"{len(self.blob_exp.extractor.tracked_addr)} interesting addresses"
             msg += "..."
 
             self.logger.info(msg)
@@ -42,13 +49,14 @@ class Stats():
     # show final metrics
     def show_final_metrics(self):
         self.logger.info(f"Finished exploring all {self.total_blocks} blocks!")
-        self.logger.info(f"Total of extracted files: {self.blob_exp.files_c}")
+        self.logger.info(f"Total of extracted files: {self.files_c}")
 
         # show extra info if these extraction modes were enabled
         if self.blob_exp.args.transactions or self.blob_exp.args.addresses:
-            self.logger.info(f"Total of transactions: {self.blob_exp.trans_c}")
+            self.logger.info(f"Total of transactions: {self.trans_c}")
         if self.blob_exp.args.addresses:
-            self.logger.info(f"Total of interesting addresses: \
-                            {len(self.blob_exp.extractor.tracked_addr)}")
+            self.logger.info(f"Total of interesting addresses: "\
+                            f"{len(self.blob_exp.extractor.tracked_addr)}")
+            self.logger.info(f"Total of files found on interesting addresses: {self.addr_file_c}")
 
         return
