883071cebff2f977d38f7f79e023d9cfd38ef37c
litneet64 <litneet64@gmail.com>
2021-03-04 03:49:18 -0300

    fix: add etherscan-python import requirement

--- etherblob/utils/args.py
+++ etherblob/utils/args.py
@@ -139,6 +139,6 @@ class Args():
                 nargs = '*', default = ["default_file_fmt"])
 
         # print version and exit
-        parser.add_argument('--version', action = 'version', version = 'EtherBlob Explorer 1.3.1')
+        parser.add_argument('--version', action = 'version', version = 'EtherBlob Explorer 1.3.2')
 
         return parser.parse_args()
