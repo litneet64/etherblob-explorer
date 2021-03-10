62132c5759b5aa4611257a0e840458a0abe251ee
litneet64 <litneet64@gmail.com>
2021-03-10 02:25:14 -0300

    fix: version semantics

--- etherblob/utils/args.py
+++ etherblob/utils/args.py
@@ -164,6 +164,6 @@ class Args():
                 nargs = '*', default = ["default_file_fmt"])
 
         # print version and exit
-        parser.add_argument('--version', action = 'version', version = 'EtherBlob Explorer 1.3.3')
+        parser.add_argument('--version', action = 'version', version = 'EtherBlob Explorer 1.4.0')
 
         return parser.parse_args()
